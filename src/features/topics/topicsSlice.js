782487042ff8270cd06fcbbea10cde9bec65aca8
Carlos Requena <101939628+carlos-req@users.noreply.github.com>
2023-02-16 23:32:16 -0500

    topicsSlice created

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const topicsSlice = createSlice({
+  name: "topics",
+  initialState: {
+    topics: {}
+  },
+  reducers: {
+    addTopic: (state, action) => {
+      const { id, name, icon } = action.payload;
+      state.topics[id] = {
+        id: id,
+        name: name,
+        icon: icon,
+        quizIds: []
+      };
+    }
+  }
+});
+
+export const { addTopic } = topicsSlice.actions;
+export const selectTopics = (state) => state.topics.topics;
+export default topicsSlice.reducer;
